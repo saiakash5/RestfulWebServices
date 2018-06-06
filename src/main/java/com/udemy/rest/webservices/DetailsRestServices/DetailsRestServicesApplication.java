8608acd9dd7a45938e2bf78e71b71fe9c3f4a2c9
Sai Akash Kuthuru <sai@Sais-MacBook-Pro.local>
2018-06-06 15:48:56 -0500

    first commit

---
+++ src/main/java/com/udemy/rest/webservices/DetailsRestServices/DetailsRestServicesApplication.java
@@ -0,0 +1,12 @@
+package com.udemy.rest.webservices.DetailsRestServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailsRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailsRestServicesApplication.class, args);
+	}
+}
